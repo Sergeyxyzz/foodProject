3469272e552f25d4dcb204c94f615961ce3daf08
Sergey <sergeyxyzz@yandex.ru>
2021-08-21 17:00:30 +0300

    add timer

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,4 @@
+// tabs
 const tabContent = document.querySelectorAll('.tabcontent'),
       tabTitle = document.querySelectorAll('.tabheader__item'),
       tabParent = document.querySelector('.tabheader__items');
@@ -37,3 +38,47 @@ const changeTabs = event => {
 }
 
 tabParent.addEventListener('click', changeTabs)
+
+// timer
+
+function timer() {
+    setInterval(timer, 1000);
+
+    const deadline = Date.parse('2022-08-23'),
+          now = Date.parse(new Date()),
+          time = deadline - now;
+
+    const seconds = Math.floor((time / 1000) % 60),
+          minutes = Math.floor((time / 1000 / 60) % 60),
+          hours = Math.floor((time / 1000 / 60 / 60) % 24),
+          days = Math.floor((time / 1000 / 60 / 60 / 24));
+    
+    const secondsHTML = document.querySelector('#seconds'),
+          minutesHTML = document.querySelector('#minutes'),
+          hoursHTML = document.querySelector('#hours'),
+          daysHTML = document.querySelector('#days');
+
+    secondsHTML.innerHTML = `${addZero(seconds)}`
+    minutesHTML.innerHTML = `${addZero(minutes)}`
+    hoursHTML.innerHTML = `${addZero(hours)}`
+    daysHTML.innerHTML = `${addZero(days)}`
+
+    function addZero (element) {
+        if (element < 10) {
+            return `0${element}`
+        } else {
+            return element
+        }
+    }
+
+    if (time <= 0) {
+        secondsHTML.innerHTML = `00`
+        minutesHTML.innerHTML = `00`
+        hoursHTML.innerHTML = `00`
+        daysHTML.innerHTML = `00`
+    }
+
+}
+
+timer()
+
