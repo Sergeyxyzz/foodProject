bf2b999dbbf4ae1c2d56197cfcd6edfc3c0f8829
Sergey <sergeyxyzz@yandex.ru>
2021-08-21 21:19:00 +0300

    add modal

--- dist/js/main.js
+++ dist/js/main.js
@@ -82,3 +82,55 @@ function timer() {
 
 timer()
 
+// modal
+
+const btnsOpenModal = document.querySelectorAll('[data-modal]'),
+      btnCloseModal = document.querySelector('[data-close]'),
+      modal = document.querySelector('.modal');
+
+
+
+const openModal = () => {
+    modal.classList.add('show')
+    modal.classList.remove('hide')
+    document.body.style.overflow = 'hidden'
+    clearInterval(modalTimerId)
+}
+
+const closeModal = () => {
+    modal.classList.add('hide')
+    modal.classList.remove('show')
+    document.body.style.overflow = ''
+}
+
+btnsOpenModal.forEach(it => {
+    it.addEventListener('click', openModal)
+})
+
+btnCloseModal.addEventListener('click', closeModal)
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        closeModal()
+    }
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape' && modal.classList.contains('show')) {
+        closeModal()
+    }
+})
+
+const modalTimerId = setTimeout(openModal, 10000)
+
+const showModalByScroll = () => {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal()
+            window.removeEventListener('scroll', showModalByScroll)
+        }
+}
+
+window.addEventListener('scroll', showModalByScroll)
+
+
+
