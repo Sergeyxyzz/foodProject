69a22160ef72cd1a657bd40eeda243433a3efd1d
Sergey <sergeyxyzz@yandex.ru>
2021-08-30 08:08:20 +0300

    add regular cutting

--- dist/js/main.js
+++ dist/js/main.js
@@ -380,11 +380,15 @@ for (let i = 0; i < slides.length; i++) {
     dots.push(dot)
 }
 
+function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '')
+}
+
 next.addEventListener('click', () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) {
         offset = 0
     } else {
-        offset += +width.slice(0, width.length - 2)
+        offset += deleteNotDigits(width)
     }
     slidesField.style.transform = `translateX(-${offset}px)`
 
@@ -406,9 +410,9 @@ next.addEventListener('click', () => {
 
 prev.addEventListener('click', () => {
     if (offset == 0) {
-        offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+        offset = deleteNotDigits(width) * (slides.length - 1)
     } else {
-        offset -= +width.slice(0, width.length - 2)
+        offset -= deleteNotDigits(width)
     }
     slidesField.style.transform = `translateX(-${offset}px)`
 
@@ -432,7 +436,7 @@ dots.forEach(it => {
         const slideTo = e.target.getAttribute('data-slide-to')
 
         slideIndex = slideTo
-        offset = +width.slice(0, width.length - 2) * (slideTo - 1)
+        offset = deleteNotDigits(width) * (slideTo - 1)
 
         slidesField.style.transform = `translateX(-${offset}px)`
 
